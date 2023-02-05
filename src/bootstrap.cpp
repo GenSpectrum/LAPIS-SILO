ec0e88d74ddc368d5ca79fd21d39328098c0256f
Alexander Taepper <alexander.taepper@gmail.com>
2023-02-05 12:08:55 +0100

    Better bootstrapping randomization and parallelization

--- src/bootstrap.cpp
+++ src/bootstrap.cpp
@@ -3,10 +3,27 @@
 //
 
 #include <silo/bootstrap.h>
+#include <silo/common/lehmer64.h>
 #include <silo/query_engine/query_engine.h>
+#include <tbb/parallel_for.h>
+
+uint64_t my_random(uint64_t s) {
+   uint64_t x = lehmer64();
+   __uint128_t m = (__uint128_t) x * (__uint128_t) s;
+   uint64_t l = (uint64_t) m;
+   if (l < s) {
+      uint64_t t = -s % s;
+      while (l < t) {
+         x = lehmer64();
+         m = (__uint128_t) x * (__uint128_t) s;
+         l = (uint64_t) m;
+      }
+   }
+   return m >> 64;
+}
 
 unsigned gen_start_N() {
-   unsigned r = rand() % 14495618;
+   unsigned r = my_random(14495618);
    std::vector<std::pair<unsigned, unsigned>> breaks = /// histogram, first is count in bucket, second is bucket upper limit
       {
          {2256125, 25},
@@ -27,7 +44,7 @@ unsigned gen_start_N() {
    }
    std::pair<unsigned, unsigned> last_br = {328928, 0};
    for (auto br : breaks) {
-      if (r < br.first) { /// # < br
+      if (r < br.first) { /// # < br -> interpolate
          return (double) (r - last_br.first) / (double) (br.first - last_br.first) * (br.second - last_br.second);
       }
    }
@@ -36,7 +53,7 @@ unsigned gen_start_N() {
 }
 
 unsigned gen_end_N() {
-   unsigned r = rand() % 14495618;
+   unsigned r = my_random(14495618);
    std::vector<std::pair<unsigned, unsigned>> breaks = /// histogram, first is count in bucket, second is bucket upper limit
       {
          {1823567, 25},
@@ -57,7 +74,7 @@ unsigned gen_end_N() {
    }
    std::pair<unsigned, unsigned> last_br = {1525153, 0};
    for (auto br : breaks) {
-      if (r < br.first) { /// # < br
+      if (r < br.first) { /// # < br -> interpolate
          return (double) (r - last_br.first) / (double) (br.first - last_br.first) * (br.second - last_br.second);
       }
    }
@@ -66,7 +83,7 @@ unsigned gen_end_N() {
 }
 
 unsigned gen_N_len() {
-   unsigned r = rand() % 77869427;
+   unsigned r = my_random(77869427);
    std::vector<std::pair<unsigned, unsigned>> breaks = /// histogram, first is count in bucket, second is bucket upper limit
       {
          {43610073, 25},
@@ -87,7 +104,7 @@ unsigned gen_N_len() {
    }
    std::pair<unsigned, unsigned> last_br = {15439497, 1};
    for (auto br : breaks) {
-      if (r < br.first) { /// # < 25
+      if (r < br.first) { /// # < br -> interpolate
          return (double) (r - last_br.first) / (double) (br.first - last_br.first) * (br.second - last_br.second);
       }
    }
@@ -96,7 +113,11 @@ unsigned gen_N_len() {
 }
 
 char sample_pos(std::vector<uint32_t>& dist) {
-   unsigned r = rand() % dist.back();
+   if (dist.back() == 0) {
+      std::cerr << "dist back is 0" << std::endl;
+      return 'N';
+   }
+   unsigned r = my_random(dist.back());
    for (unsigned symbol = 0; symbol < dist.size(); ++symbol) {
       if (r < dist[symbol]) {
          return silo::symbol_rep[symbol];
@@ -109,7 +130,7 @@ char sample_pos(std::vector<uint32_t>& dist) {
 void gen_genome(char* ret, std::vector<std::vector<uint32_t>>& dist) {
    unsigned start_N = gen_start_N();
    unsigned end_N = gen_end_N();
-   unsigned num_inner_N_runs = rand() % 12;
+   unsigned num_inner_N_runs = my_random(12);
 
    for (unsigned i = 0; i < start_N; ++i)
       ret[i] = 'N';
@@ -129,7 +150,7 @@ void gen_genome(char* ret, std::vector<std::vector<uint32_t>>& dist) {
       if (inner_len <= N_run_length) {
          continue;
       }
-      unsigned offset = (rand() % (inner_len - N_run_length)) + start_N + 1;
+      unsigned offset = (my_random(inner_len - N_run_length)) + start_N + 1;
       for (unsigned i = offset; i < offset + N_run_length; ++i) {
          ret[i] = 'N';
       }
@@ -137,42 +158,45 @@ void gen_genome(char* ret, std::vector<std::vector<uint32_t>>& dist) {
 }
 
 int bootstrap(const silo::Database& db, std::string& out_dir, unsigned seed, unsigned factor) {
-   srand(seed);
    unsigned pango_count = db.dict->get_pango_count();
    unsigned epi_factor = pango_count * factor;
-   for (unsigned pango_id = 0; pango_id < pango_count; pango_id++) {
-      auto predicate = std::make_unique<silo::PangoLineageEx>(pango_id, false);
-      auto part_filters = silo::execute_predicate(db, predicate.get());
-      unsigned count = 0;
-      for (auto& f : part_filters) count += f.getAsConst()->cardinality();
-      auto dist = silo::execute_all_dist(db, part_filters);
-      for (unsigned rep = 0; rep < factor; rep++) {
-         std::string file_name = out_dir + std::to_string(pango_id) + "_" + std::to_string(rep) + "_" + db.dict->get_pango(pango_id) + ".fasta";
-         /// Generate output file
-         {
-            std::ofstream out_file(file_name);
-            if (!out_file || out_file.fail()) {
-               std::cerr << "Could not open output file " << file_name << std::endl;
-               return -1;
+   tbb::parallel_for(tbb::blocked_range<unsigned>(0, pango_count), [&](tbb::blocked_range<unsigned> local) {
+      for (auto pango_id = local.begin(); pango_id < local.end(); pango_id++) {
+         lehmer64_seed(seed * pango_id);
+         auto predicate = std::make_unique<silo::PangoLineageEx>(pango_id, false);
+         auto part_filters = silo::execute_predicate(db, predicate.get());
+         unsigned count = 0;
+         for (auto& f : part_filters) count += f.getAsConst()->cardinality();
+         auto dist = silo::execute_all_dist(db, part_filters);
+         char genome[silo::genomeLength]; // Buffer
+         for (unsigned rep = 0; rep < factor; rep++) {
+            std::string file_name = out_dir + std::to_string(pango_id) + "_" + std::to_string(rep) + "_" + db.dict->get_pango(pango_id) + ".fasta";
+            /// Generate output file
+            {
+               std::ofstream out_file(file_name);
+               if (!out_file || out_file.fail()) {
+                  std::cerr << "Could not open output file " << file_name << std::endl;
+                  return -1;
+               }
+               for (unsigned i = 0; i < count; i++) {
+                  out_file << "EPI_ISL_" << std::to_string(i * epi_factor + rep * pango_count + pango_id) << '\n';
+                  gen_genome(genome, dist);
+                  out_file << genome << '\n';
+               }
             }
-            for (unsigned i = 0; i < count; i++) {
-               out_file << "EPI_ISL_" << std::to_string(i * epi_factor + rep * pango_count + pango_id) << '\n';
-               char genome[silo::genomeLength];
-               gen_genome(genome, dist);
-               out_file << genome << '\n';
-            }
-         }
-         /// xz compress output file
-         {
-            std::string compress_command = "xz -T0 -z " + file_name;
-            int ret_code = system(compress_command.c_str());
-            if (ret_code) {
-               std::cerr << "Command to compress generated file " << file_name << ":\n"
-                         << compress_command << '\n'
-                         << "Returned with non zero return code " << ret_code << std::endl;
+            /// xz compress output file
+            {
+               std::string compress_command = "xz -T0 -z " + file_name;
+               int ret_code = system(compress_command.c_str());
+               if (ret_code) {
+                  std::cerr << "Command to compress generated file " << file_name << ":\n"
+                            << compress_command << '\n'
+                            << "Returned with non zero return code " << ret_code << std::endl;
+               }
             }
          }
       }
-   }
+      return 0;
+   });
    return 0;
 }
