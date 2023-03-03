f8ce6cf6acf9817be08a7037dcb4ebdaa227f759
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2023-03-03 10:19:47 +0100

    build: add gtest and gmock

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,8 @@ RUN apk update && \
         readline-dev=8.2.0-r0 \
         xz-dev=5.2.9-r0 \
         rapidjson-dev=1.1.0-r4 \
-        poco-dev=1.12.2-r1
+        poco-dev=1.12.2-r1 \
+        gtest-dev=1.12.1-r0
 
 WORKDIR /src
 COPY . .
--- test/resolve_alias_test.cpp
+++ test/resolve_alias_test.cpp
@@ -1,39 +1,35 @@
-//
-// Created by Alexander Taepper on 06.10.22.
-//
-#include <cassert>
+#include "gtest/gtest.h"
 #include <silo/common/silo_symbols.h>
 
-void resolve_alias_test() {
-   std::unordered_map<std::string, std::string> alias_key;
-   alias_key["X"] = "A";
-   alias_key["XY"] = "A.1";
+struct TestParameter {
+   std::string input;
+   std::string expected_result;
+};
 
-   std::string test_empty;
-   std::string test_empty_res = silo::resolve_alias(alias_key, test_empty);
-   assert(test_empty_res.empty());
+class ResolveAliasTestFixture : public ::testing::TestWithParam<TestParameter> {
+   protected:
+   const std::unordered_map<std::string, std::string> alias_map =
+      {
+         {"X", "A"},
+         {"XY", "A.1"}};
+};
 
-   std::string test1 = "Test";
-   std::string test1_res = silo::resolve_alias(alias_key, test1);
-   assert(test1_res == "Test");
+TEST_P(ResolveAliasTestFixture, should_return_expected_resolved_alias) {
+   const auto test_parameter = GetParam();
 
-   std::string test2 = "X";
-   std::string test2_res = silo::resolve_alias(alias_key, test2);
-   assert(test2_res == "A");
+   const auto result = silo::resolve_alias(alias_map, test_parameter.input);
 
-   std::string test3 = "XY";
-   std::string test3_res = silo::resolve_alias(alias_key, test3);
-   assert(test3_res == "A.1");
-
-   std::string test4 = "X.1.1";
-   std::string test4_res = silo::resolve_alias(alias_key, test4);
-   assert(test4_res == "A.1.1");
-
-   std::string test5 = "XYX.1.1";
-   std::string test5_res = silo::resolve_alias(alias_key, test5);
-   assert(test5_res == "XYX.1.1");
-
-   std::string test6 = ".X";
-   std::string test6_res = silo::resolve_alias(alias_key, test6);
-   assert(test6_res == ".X");
+   ASSERT_EQ(result, test_parameter.expected_result);
 }
+
+INSTANTIATE_TEST_SUITE_P(
+   resolve_alias_test,
+   ResolveAliasTestFixture,
+   ::testing::Values(
+      TestParameter{"", ""},
+      TestParameter{"SomeNotListedAlias", "SomeNotListedAlias"},
+      TestParameter{"X", "A"},
+      TestParameter{"XY", "A.1"},
+      TestParameter{"X.1.1", "A.1.1"},
+      TestParameter{"XYX.1.1", "XYX.1.1"},
+      TestParameter{".X", ".X"}));
