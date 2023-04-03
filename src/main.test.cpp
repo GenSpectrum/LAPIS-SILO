1fc2a9a6fb816c4cd58d3644dc98ef7f46b42dfc
Alexander Taepper <alexander.taepper@gmail.com>
2023-04-03 14:05:51 +0200

    fix: correct placement of endToEnd tests

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -1,10 +1,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <silo/common/log.h>
+#include <spdlog/sinks/null_sink.h>
 #include <spdlog/spdlog.h>
 
 int main(int argc, char* argv[]) {
    spdlog::set_level(spdlog::level::off);
-
+   spdlog::null_logger_mt(silo::PERFORMANCE_LOGGER_NAME);
    ::testing::InitGoogleMock(&argc, argv);
    return RUN_ALL_TESTS();
 }
