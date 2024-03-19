611b8564d5a0b84faab271665c6590b2e618c24a
Christian Jaeger <ch@christianjaeger.ch>
2024-03-19 14:01:07 +0100

    add boolean columns: add optional_bool

---
+++ src/silo/common/optional_bool.cpp
@@ -0,0 +1,57 @@
+#include "silo/common/optional_bool.h"
+
+#include <cstdlib>
+
+namespace silo::common {
+
+using optional_bool::Representation;
+
+OptionalBool::OptionalBool() {
+   representation = Representation::NONE;
+}
+
+OptionalBool::OptionalBool(bool value) {
+   representation = value ? Representation::TRUE : Representation::FALSE;
+}
+
+OptionalBool::OptionalBool(std::optional<bool> value) {
+   if (value.has_value()) {
+      representation = value ? Representation::TRUE : Representation::FALSE;
+   } else {
+      representation = Representation::NONE;
+   }
+}
+
+std::strong_ordering OptionalBool::operator<=>(const OptionalBool& other) const {
+   return representation <=> other.representation;
+}
+
+bool OptionalBool::isNull() const noexcept {
+   return representation == Representation::NONE;
+}
+
+std::optional<bool> OptionalBool::value() const noexcept {
+   switch (representation) {
+      case Representation::NONE:
+         return std::nullopt;
+      case Representation::FALSE:
+         return false;
+      case Representation::TRUE:
+         return true;
+   }
+   abort();
+}
+
+std::string_view OptionalBool::asStr() const noexcept {
+   switch (representation) {
+      case Representation::NONE:
+         return "null";
+      case Representation::FALSE:
+         return "false";
+      case Representation::TRUE:
+         return "true";
+   }
+   abort();
+}
+
+}  // namespace silo::common
