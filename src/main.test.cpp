418159fc0ce817e7f4caec07201fde6be6fbabb6
Christian Jaeger <ch@christianjaeger.ch>
2024-03-25 10:57:07 +0000

    silo_test: check if running in repository root

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -2,10 +2,19 @@
 #include <gtest/gtest.h>
 #include <spdlog/sinks/null_sink.h>
 #include <spdlog/spdlog.h>
+#include <filesystem>
 
 #include "silo/common/log.h"
 
 int main(int argc, char* argv[]) {
+   try {
+      if (!std::filesystem::exists("testBaseData/exampleDataset")) {
+         throw std::runtime_error("must be run in root of repository");
+      }
+   } catch (std::exception& e) {
+      SPDLOG_ERROR(e.what());
+      return 1;
+   }
    spdlog::set_level(spdlog::level::info);
    spdlog::null_logger_mt(silo::PERFORMANCE_LOGGER_NAME);
    ::testing::InitGoogleMock(&argc, argv);
