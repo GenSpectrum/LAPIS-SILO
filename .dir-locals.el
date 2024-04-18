dbcaeb01003ca056e5514f9a1334753257cd1095
Christian Jaeger <ch@christianjaeger.ch>
2024-04-18 13:37:39 +0200

    chore: add .dir-locals.el for project-specific Emacs settings
    Refs: #391

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;; The 'nil' configuration applies to all modes.
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 2) ;; used by what?
+         (js-indent-level . 2)
+         (c-basic-offset . 3)
+         )))
