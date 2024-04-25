70523298b0cbb513f703e567bd9db9281d29f871
Alexander Taepper <alexander.taepper@gmail.com>
2024-04-25 16:03:06 +0200

    feat: tidying up CLI and file configuration for runtime config. Added option for specifying the port

--- include/silo_api/runtime_config.h
+++ include/silo_api/runtime_config.h
@@ -3,12 +3,25 @@
 #include <filesystem>
 #include <optional>
 
+#include <Poco/Util/AbstractConfiguration.h>
+
+#include "silo/preprocessing/preprocessing_config.h"
+
 namespace silo_api {
 
-struct RuntimeConfig {
-   std::optional<std::filesystem::path> data_directory;
+static const std::string DATA_DIRECTORY_OPTION = "dataDirectory";
+static const std::string MAX_CONNECTIONS_OPTION = "maxQueuedHttpConnections";
+static const std::string PARALLEL_THREADS_OPTION = "threadsForHttpConnections";
+static const std::string PORT_OPTION = "port";
 
-   static RuntimeConfig readFromFile(const std::filesystem::path& config_path);
+struct RuntimeConfig {
+   std::filesystem::path data_directory = silo::preprocessing::DEFAULT_OUTPUT_DIRECTORY.directory;
+   int32_t max_connections = 64;
+   int32_t parallel_threads = 4;
+   uint16_t port = 8081;
+
+   void overwriteFromFile(const std::filesystem::path& config_path);
+   void overwriteFromCommandLineArguments(const Poco::Util::AbstractConfiguration& config);
 };
 
 }  // namespace silo_api
--- src/silo_api/runtime_config.cpp
+++ src/silo_api/runtime_config.cpp
@@ -6,34 +6,75 @@
 #include <spdlog/spdlog.h>
 #include <yaml-cpp/yaml.h>
 
-namespace YAML {
-
-template <>
-struct convert<silo_api::RuntimeConfig> {
-   static bool decode(const Node& node, silo_api::RuntimeConfig& config) {
-      config = silo_api::RuntimeConfig{
-         node["dataDirectory"]
-            ? std::optional<std::filesystem::path>(node["dataDirectory"].as<std::string>())
-            : std::nullopt
-      };
-
-      return true;
-   }
-};
-
-}  // namespace YAML
-
 namespace silo_api {
 
-RuntimeConfig RuntimeConfig::readFromFile(const std::filesystem::path& config_path) {
+void RuntimeConfig::overwriteFromFile(const std::filesystem::path& config_path) {
    SPDLOG_INFO("Reading runtime config from {}", config_path.string());
 
    try {
-      return YAML::LoadFile(config_path.string()).as<RuntimeConfig>();
+      YAML::Node node = YAML::LoadFile(config_path.string());
+      if (node[DATA_DIRECTORY_OPTION]) {
+         SPDLOG_DEBUG(
+            "Using dataDirectory passed via config file: {}",
+            node[DATA_DIRECTORY_OPTION].as<std::string>()
+         );
+         data_directory = node[DATA_DIRECTORY_OPTION].as<std::string>();
+      }
+      if (node[MAX_CONNECTIONS_OPTION]) {
+         SPDLOG_DEBUG(
+            "Using maximum queued http connections passed via config file: {}",
+            node[MAX_CONNECTIONS_OPTION].as<int32_t>()
+         );
+         max_connections = node[MAX_CONNECTIONS_OPTION].as<int32_t>();
+      }
+      if (node[PARALLEL_THREADS_OPTION]) {
+         SPDLOG_DEBUG(
+            "Using parallel threads for accepting http connections as passed via config file: {}",
+            node[PARALLEL_THREADS_OPTION].as<int32_t>()
+         );
+         parallel_threads = node[PARALLEL_THREADS_OPTION].as<int32_t>();
+      }
+      if (node[PORT_OPTION]) {
+         SPDLOG_DEBUG("Using port passed via config file: {}", node[PORT_OPTION].as<uint16_t>());
+         port = node[PORT_OPTION].as<uint16_t>();
+      }
    } catch (const YAML::Exception& e) {
       throw std::runtime_error(
          "Failed to read runtime config from " + config_path.string() + ": " + std::string(e.what())
       );
+   }
+}
+
+void RuntimeConfig::overwriteFromCommandLineArguments(
+   const Poco::Util::AbstractConfiguration& config
+) {
+   if (config.hasProperty(DATA_DIRECTORY_OPTION)) {
+      SPDLOG_DEBUG(
+         "Using dataDirectory passed via command line argument: {}",
+         config.getString(DATA_DIRECTORY_OPTION)
+      );
+      data_directory = config.getString(DATA_DIRECTORY_OPTION);
+   }
+   if (config.hasProperty(MAX_CONNECTIONS_OPTION)) {
+      SPDLOG_DEBUG(
+         "Using maximum queued http connections passed via command line argument: {}",
+         config.getInt(MAX_CONNECTIONS_OPTION)
+      );
+      max_connections = config.getInt(MAX_CONNECTIONS_OPTION);
+   }
+   if (config.hasProperty(PARALLEL_THREADS_OPTION)) {
+      SPDLOG_DEBUG(
+         "Using parallel threads for accepting http connections as passed via command line "
+         "argument: {}",
+         config.getInt(PARALLEL_THREADS_OPTION)
+      );
+      parallel_threads = config.getInt(PARALLEL_THREADS_OPTION);
+   }
+   if (config.hasProperty(PORT_OPTION)) {
+      SPDLOG_DEBUG(
+         "Using port passed via command line argument: {}", config.getString(PORT_OPTION)
+      );
+      port = config.getUInt(PORT_OPTION);
    }
 }
 
