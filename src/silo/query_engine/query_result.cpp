c38ae211374ff19b9d6e33c0f5502a4737d65afd
Christian Jaeger <ch@christianjaeger.ch>
2024-05-02 21:10:38 +0200

    feat: streaming: QueryResult: iterate via index
    Because the iterator based version doesn't stop.
    
    Refs: #394

--- src/silo/query_engine/query_result.cpp
+++ src/silo/query_engine/query_result.cpp
@@ -11,30 +11,29 @@ namespace silo::query_engine {
 void QueryResult::clear() {
    query_result_chunk_.clear();
    get_chunk_ = [](std::vector<QueryResultEntry>& /*query_result_chunk*/) {};
-   iter_ = query_result_chunk_.begin();
+   i_ = 0;
 }
 
 std::optional<std::reference_wrapper<const QueryResultEntry>> QueryResult::next() {
+   auto siz = query_result_chunk_.size();
    SPDLOG_DEBUG(
-      "DEBUG: next called, is_materialized_ = {}, chunk len = {}",
-      is_materialized_,
-      query_result_chunk_.size()
+      "next called, i = {}, is_materialized_ = {}, chunk size() = {}", i_, is_materialized_, siz
    );
-   if (iter_ == query_result_chunk_.end()) {
+   if (i_ >= siz) {
       SPDLOG_DEBUG("reached the end of last chunk");
       query_result_chunk_.clear();
       SPDLOG_DEBUG("cleared vector");
       get_chunk_(query_result_chunk_);
-      SPDLOG_DEBUG("DEBUG: returned from get_chunk_");
-      iter_ = query_result_chunk_.begin();
-      SPDLOG_DEBUG("DEBUG: set iterator to begin");
-      if (query_result_chunk_.empty()) {
+      i_ = 0;
+      siz = query_result_chunk_.size();
+      SPDLOG_DEBUG("returned from get_chunk_, chunk size() = {}", siz);
+      if (siz == 0) {
          SPDLOG_DEBUG("returning {} from next");
          return {};
       }
    }
-   const QueryResultEntry& ref = *iter_;
-   ++iter_;
+   const QueryResultEntry& ref = query_result_chunk_[i_];
+   ++i_;  // can't overflow because i_ >= siz will always be true before.
    SPDLOG_DEBUG("returning ref from next");
    return {std::cref(ref)};
 }
