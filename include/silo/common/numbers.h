f73af154dc0665c85be03098e9901503b104aacb
Christian Jaeger <ch@christianjaeger.ch>
2024-05-16 16:30:47 +0200

    feat: streaming: numbers: add `inc`, use it
    Refs: #394

--- include/silo/common/numbers.h
+++ include/silo/common/numbers.h
@@ -4,3 +4,6 @@
 
 /// Abort if `val` isn't in uint32_t range.
 uint32_t uint64ToUint32(uint64_t val);
+
+/// Abort on overflow
+uint32_t inc(uint32_t val);
--- src/silo/common/numbers.cpp
+++ src/silo/common/numbers.cpp
@@ -6,3 +6,8 @@ uint32_t uint64ToUint32(uint64_t val) {
    assert(val <= UINT32_MAX);
    return val;
 }
+
+uint32_t inc(uint32_t val) {
+   assert(val < UINT32_MAX);
+   return val + 1;
+}
