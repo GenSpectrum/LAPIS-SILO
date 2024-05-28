70d22fb01f9669ac439c81bd8cb544021010c217
Christian Jaeger <ch@christianjaeger.ch>
2024-05-28 15:37:33 +0200

    fix: numbers: throw exception instead of aborting
    We're not careful enough (and these functions aren't even meant for
    those careful cases) to preclude bugs.
    
    Refs: #394

--- include/silo/common/numbers.h
+++ include/silo/common/numbers.h
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 
-/// Abort if `val` isn't in uint32_t range.
+/// Throws `std::overflow_error` if `val` isn't in uint32_t range.
 uint32_t uint64ToUint32(uint64_t val);
 
-/// Abort on overflow
+/// Throws `std::overflow_error` on overflow
 uint32_t inc(uint32_t val);
