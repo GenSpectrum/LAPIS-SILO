8920d5ff7af6906aa55f4a5b034e11c631df2955
Christian Jaeger <ch@christianjaeger.ch>
2024-05-31 15:27:32 +0200

    feat: add materializationCutoff option
    - Add struct `QueryOptions` and add it as field to `RuntimeConfig`.
    
    - The bulk of `RuntimeConfig` becomes `ApiOptions`; `ApiOptions` was
      already introduced in commit c8cf88e74999a ("refactor: enable
      YAMLFiles as ConfigSources to have substructs").
    
    - add `overwrite` methods to ApiOptions and QueryOptions (and
      RuntimeConfig as a delegate)
    
    Refs: #633, #524

--- src/silo/config/runtime_config.cpp
+++ src/silo/config/runtime_config.cpp
@@ -9,10 +9,10 @@
 
 namespace silo::config {
 
-void RuntimeConfig::overwrite(const silo::config::AbstractConfigSource& config) {
+void ApiOptions::overwrite(const silo::config::AbstractConfigSource& config) {
    if (auto value = config.getString(DATA_DIRECTORY_OPTION)) {
       SPDLOG_DEBUG("Using dataDirectory passed via {}: {}", config.configType(), *value);
-      api_options.data_directory = *value;
+      data_directory = *value;
    }
    if (auto value = config.getInt32(MAX_CONNECTIONS_OPTION)) {
       SPDLOG_DEBUG(
@@ -21,7 +21,7 @@ void RuntimeConfig::overwrite(const silo::config::AbstractConfigSource& config) 
          config.configType(),
          *value
       );
-      api_options.max_connections = *value;
+      max_connections = *value;
    }
    if (auto value = config.getInt32(PARALLEL_THREADS_OPTION)) {
       SPDLOG_DEBUG(
@@ -30,13 +30,13 @@ void RuntimeConfig::overwrite(const silo::config::AbstractConfigSource& config) 
          config.configType(),
          *value
       );
-      api_options.parallel_threads = *value;
+      parallel_threads = *value;
    }
    if (auto value = config.getUInt32(PORT_OPTION)) {
       SPDLOG_DEBUG(
          "Using {} passed via {}: {}", PORT_OPTION.toString(), config.configType(), *value
       );
-      api_options.port = *value;
+      port = *value;
    }
    if (auto value = config.getInt32(ESTIMATED_STARTUP_TIME_IN_MINUTES_OPTION)) {
       SPDLOG_DEBUG(
@@ -46,8 +46,24 @@ void RuntimeConfig::overwrite(const silo::config::AbstractConfigSource& config) 
          *value
       );
       const std::chrono::minutes minutes = std::chrono::minutes(*value);
-      api_options.estimated_startup_end = std::chrono::system_clock::now() + minutes;
+      estimated_startup_end = std::chrono::system_clock::now() + minutes;
    }
+}
+
+void QueryOptions::overwrite(const silo::config::AbstractConfigSource& config) {
+   if (auto value = config.getUInt64(MATERIALIZATION_CUTOFF_OPTION)) {
+      SPDLOG_DEBUG(
+         "Using {} passed via {}: {}",
+         MATERIALIZATION_CUTOFF_OPTION.toString(),
+         config.configType(),
+         *value
+      );
+      materialization_cutoff = *value;
+   }
+}
+
+void RuntimeConfig::overwrite(const silo::config::AbstractConfigSource& config) {
+   api_options.overwrite(config);
 }
 
 }  // namespace silo::config
