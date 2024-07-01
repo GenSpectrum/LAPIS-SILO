a2d9e5c109e849a845958b0b43156eecd4d40efe
Christian Jaeger <ch@christianjaeger.ch>
2024-07-01 17:26:40 +0200

    refactor: numbers: replace `uint64ToUint32` with `boost::numeric_cast`
    Refs: #112

--- src/silo/common/numbers.cpp
+++ src/silo/common/numbers.cpp
@@ -2,15 +2,6 @@
 
 #include <stdexcept>
 
-#include <fmt/format.h>
-
-uint32_t uint64ToUint32(uint64_t val) {
-   if (val <= UINT32_MAX) {
-      return val;
-   }
-   throw std::overflow_error{fmt::format("number outside uint32 range: {}", val)};
-}
-
 uint32_t inc(uint32_t val) {
    if (val < UINT32_MAX) {
       return val + 1;
