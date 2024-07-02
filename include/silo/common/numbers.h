3d19469f72736f9482f0f4126bb5e75d0cc33752
Christian Jaeger <ch@christianjaeger.ch>
2024-07-02 11:24:52 +0200

    refactor: move uint64ToUint32 to silo/common/numbers
    Refs: #112

---
+++ include/silo/common/numbers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cstdint>
+
+/// Abort if `val` isn't in uint32_t range.
+uint32_t uint64ToUint32(uint64_t val);
---
+++ src/silo/common/numbers.cpp
@@ -0,0 +1,8 @@
+#include "silo/common/numbers.h"
+
+#include <cassert>
+
+uint32_t uint64ToUint32(uint64_t val) {
+   assert(val <= UINT32_MAX);
+   return val;
+}
