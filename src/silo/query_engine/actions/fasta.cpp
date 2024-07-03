f73106612f9d39730ec00c142dbf8ddbe6097550
Christian Jaeger <ch@christianjaeger.ch>
2024-07-03 14:22:25 +0200

    refactor: numbers: rename `inc` to `add1`
    Refs: #112

--- src/silo/query_engine/actions/fasta.cpp
+++ src/silo/query_engine/actions/fasta.cpp
@@ -306,7 +306,7 @@ QueryResult Fasta::execute(const Database& database, std::vector<OperatorResult>
 
             *remaining_result_row_indices =
                remaining_result_row_indices->drop(result_row_indices.size());
-            bitmap->removeRange(0, inc(last_row_id));
+            bitmap->removeRange(0, add1(last_row_id));
             return;
          }
       }
