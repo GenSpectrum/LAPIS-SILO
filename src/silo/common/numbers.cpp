4d7bee241775aa535ce2a65eebb5063c4dbc1478
Christian Jaeger <ch@christianjaeger.ch>
2024-07-03 14:21:05 +0200

    refactor: numbers: throw exception instead of aborting
    We're not careful enough (and these functions aren't even meant for
    those careful cases) to preclude bugs.
    
    Refs: #112

--- src/silo/common/numbers.cpp
+++ src/silo/common/numbers.cpp
@@ -1,13 +1,19 @@
 #include "silo/common/numbers.h"
 
-#include <cassert>
+#include <stdexcept>
+
+#include <fmt/format.h>
 
 uint32_t uint64ToUint32(uint64_t val) {
-   assert(val <= UINT32_MAX);
-   return val;
+   if (val <= UINT32_MAX) {
+      return val;
+   }
+   throw std::overflow_error{fmt::format("number outside uint32 range: {}", val)};
 }
 
 uint32_t inc(uint32_t val) {
-   assert(val < UINT32_MAX);
-   return val + 1;
+   if (val < UINT32_MAX) {
+      return val + 1;
+   }
+   throw std::overflow_error{"inc: uint32 number overflow"};
 }
