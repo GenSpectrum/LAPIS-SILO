51fb46c83aa56f966e1de8587240269468a16234
Christian Jaeger <ch@christianjaeger.ch>
2024-07-24 11:37:20 +0200

    refactor: action: use more appropriate new `QueryEvaluationException`

--- src/silo/query_engine/query_parse_exception.cpp
+++ src/silo/query_engine/query_parse_exception.cpp
@@ -4,6 +4,23 @@
 #include <string>
 
 namespace silo {
-[[maybe_unused]] QueryParseException::QueryParseException(const std::string& error_message)
+
+QueryException::QueryException(const std::string& error_message)
     : std::runtime_error(error_message.c_str()) {}
+
+[[maybe_unused]] QueryParseException::QueryParseException(const std::string& error_message)
+    : QueryException(error_message) {}
+
+std::string_view QueryParseException::duringString() const {
+   return "parsing";
+}
+
+[[maybe_unused]] QueryEvaluationException::QueryEvaluationException(const std::string& error_message
+)
+    : QueryException(error_message) {}
+
+std::string_view QueryEvaluationException::duringString() const {
+   return "evaluation";
+}
+
 }  // namespace silo
