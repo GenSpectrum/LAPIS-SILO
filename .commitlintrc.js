2e2a31db969b50d0ba6783c1a6c4661e0ba3d5e5
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-09-03 09:17:43 +0200

    chore: disable commitlint body-max-line-length, so that dependabot commits pass

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -6,7 +6,7 @@ import { RuleConfigSeverity } from "@commitlint/types";
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [RuleConfigSeverity.Error, "always", 200],
+    "body-max-line-length": [RuleConfigSeverity.Disabled],
     "header-max-length": [RuleConfigSeverity.Error, "always", 200],
     "footer-max-line-length": [RuleConfigSeverity.Disabled],
   },
