8cf6d3a955fcb5ae74fbf2d5888313660de3aee1
Alexander Taepper <alexander.taepper@gmail.com>
2024-10-08 15:40:32 +0200

    chore: remove helper script, which generates lineage definitions file

--- scripts/generate_new_lineage_definitions.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-curl https://raw.githubusercontent.com/cov-lineages/pango-designation/refs/heads/master/pango_designation/alias_key.json > alias_key.json 
-curl https://raw.githubusercontent.com/cov-lineages/pango-designation/refs/heads/master/lineages.csv > lineages.csv
-python3 uniqueValuesFromColumn.py lineages.csv 1 lineage_keys.csv
-python3 alias2lineageDefinitions.py alias_key.json lineage_keys.csv > lineage_definitions.yaml
