3cd36d6783e95222dbb47704ebbf160d0aeedb84
Alexander Taepper <alexander.taepper@bsse.ethz.ch>
2024-10-28 10:08:37 +0000

    ..

--- src/silo/test/query_fixture.test.cpp
+++
@@ -1,7 +0,0 @@
-#include "silo/test/query_fixture.test.h"
-
-std::string silo::test::printScenarioName(
-   const ::testing::TestParamInfo<QueryTestScenario>& scenario
-) {
-   return scenario.param.name;
-}
