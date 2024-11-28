e58f5feaea55624a86bf851fa1a5d4fe21a2be3b
Alexander Taepper <alexander.taepper@gmail.com>
2024-11-28 17:10:42 +0100

    also add yaml errors and refactor

--- src/config/config_value.cpp
+++ src/config/config_value.cpp
@@ -50,63 +50,54 @@ std::string ConfigValue::toString() const {
 }
 
 ConfigValue ConfigValueSpecification::getValueFromString(std::string value_string) const {
-   switch (type) {
-      case ConfigValueType::STRING:
-         return ConfigValue::fromString(value_string);
-      case ConfigValueType::PATH: {
-         return ConfigValue::fromPath(value_string);
-      }
-      case ConfigValueType::UINT32:
-         try {
+   try {
+      switch (type) {
+         case ConfigValueType::STRING:
+            return ConfigValue::fromString(value_string);
+         case ConfigValueType::PATH: {
+            return ConfigValue::fromPath(value_string);
+         }
+         case ConfigValueType::UINT32: {
             // Because boost does not error on negative numbers
             if (value_string.starts_with('-')) {
                throw ConfigException(
-                  fmt::format("cannot parse: {} as {}", value_string, configValueTypeToString(type))
+                  fmt::format("cannot parse negative number '{}' as unsigned type {}", value_string, configValueTypeToString(type))
                );
             }
             const auto parsed_unsigned = boost::lexical_cast<uint32_t>(value_string);
             return ConfigValue::fromUint32(parsed_unsigned);
-         } catch (boost::bad_lexical_cast& _) {
-            throw ConfigException(
-               fmt::format("cannot parse: {} as {}", value_string, configValueTypeToString(type))
-            );
          }
-      case ConfigValueType::UINT16:
-         try {
-            // Because boost does not error on negative numbers
-            if (value_string.starts_with('-')) {
-               throw ConfigException(
-                  fmt::format("cannot parse: {} as {}", value_string, configValueTypeToString(type))
-               );
+         case ConfigValueType::UINT16:
+            {
+               // Because boost does not error on negative numbers
+               if (value_string.starts_with('-')) {
+                  throw ConfigException(
+                     fmt::format("cannot parse negative number '{}' as unsigned type {}", value_string, configValueTypeToString(type))
+                  );
+               }
+               const auto parsed_unsigned = boost::lexical_cast<uint16_t>(value_string);
+               return ConfigValue::fromUint16(parsed_unsigned);
             }
-            const auto parsed_unsigned = boost::lexical_cast<uint16_t>(value_string);
-            return ConfigValue::fromUint16(parsed_unsigned);
-         } catch (boost::bad_lexical_cast& _) {
-            throw ConfigException(
-               fmt::format("cannot parse: {} as {}", value_string, configValueTypeToString(type))
-            );
-         }
-      case ConfigValueType::INT32:
-         try {
-            const auto parsed_signed = boost::lexical_cast<int32_t>(value_string);
-            return ConfigValue::fromInt32(parsed_signed);
-         } catch (boost::bad_lexical_cast& _) {
-            throw ConfigException(
-               fmt::format("cannot parse: {} as {}", value_string, configValueTypeToString(type))
-            );
-         }
-      case ConfigValueType::BOOL:
-         if (value_string == "true" || value_string == "1") {
-            return ConfigValue::fromBool(true);
-         }
-         if (value_string == "false" || value_string == "0") {
-            return ConfigValue::fromBool(false);
-         }
-         throw ConfigException(fmt::format(
-            "cannot parse '{}' as boolean: only 0,1,false,true are allowed", value_string
-         ));
+         case ConfigValueType::INT32:
+            {
+               const auto parsed_signed = boost::lexical_cast<int32_t>(value_string);
+               return ConfigValue::fromInt32(parsed_signed);
+            }
+         case ConfigValueType::BOOL:
+            if (value_string == "true" || value_string == "1") {
+               return ConfigValue::fromBool(true);
+            }
+            if (value_string == "false" || value_string == "0") {
+               return ConfigValue::fromBool(false);
+            }
+      }
+      SILO_UNREACHABLE();
    }
-   SILO_UNREACHABLE();
+   catch (boost::bad_lexical_cast& _) {
+      throw ConfigException(
+         fmt::format("cannot parse '{}' as {}", value_string, configValueTypeToString(type))
+      );
+   }
 }
 
 }  // namespace silo::config
