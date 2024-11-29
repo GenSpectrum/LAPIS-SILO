0dc00ee41fc1cb1b7e4b85956148aeedec4bcae3
Christian Jaeger <ch@christianjaeger.ch>
2024-11-29 09:59:59 +0100

    -staticetc

--- include/config/source/environment_variables.h
+++ include/config/source/environment_variables.h
@@ -30,24 +30,24 @@ class EnvironmentVariables : public ConfigSource {
        : alist(std::move(alist_)),
          allow_list(std::move(allow_list_)){};
 
-   explicit EnvironmentVariables(){};
+   explicit EnvironmentVariables() = default;
+
+   [[nodiscard]] static constexpr std::string_view errorContext() {
+      return "environment variables";
+   };
 
   public:
-   [[nodiscard]] VerifiedConfigSource verify(const ConfigSpecification& config_specification
-   ) const override;
-
    static EnvironmentVariables newWithAllowListAndEnv(
       const std::vector<std::string>& allow_list,
       const char* const* envp
    );
 
-   [[nodiscard]] constexpr std::string_view errorContext() const {
-      return "environment variables";
-   };
-
    static std::string configKeyPathToString(const ConfigKeyPath& key_path);
 
    static AmbiguousConfigKeyPath stringToConfigKeyPath(const std::string& key_path_string);
+
+   [[nodiscard]] VerifiedConfigSource verify(const ConfigSpecification& config_specification
+   ) const override;
 };
 
 }  // namespace silo::config
