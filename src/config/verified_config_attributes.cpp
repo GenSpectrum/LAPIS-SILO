7fab38302dbb3ec5395746fcc8fd8eb06a2342f9
Alexander Taepper <alexander.taepper@gmail.com>
2024-12-09 14:14:08 +0100

    - extract common code

--- src/config/verified_config_attributes.cpp
+++ src/config/verified_config_attributes.cpp
@@ -6,15 +6,15 @@
 
 namespace silo::config {
 
-std::optional<std::string> VerifiedConfigAttributes::getString(const ConfigKeyPath& config_key_path
-) const {
+template <typename T, ConfigValueType ExpectedType>
+std::optional<T> getValue(const ConfigKeyPath& config_key_path, const std::unordered_map<ConfigKeyPath, ConfigValue>& config_values) {
    auto value_it = config_values.find(config_key_path);
    if (value_it != config_values.end()) {
       const ConfigValue& value = value_it->second;
-      if (value.getValueType() != ConfigValueType::STRING) {
+      if (value.getValueType() != ExpectedType) {
          SILO_PANIC(
-            "Called getString on a ConfigKeyPath ('{}') that belongs to a value of another "
-            "type ({}).",
+            "Called getValue with type {} on a ConfigKeyPath ('{}') that belongs to a value of another type ({}).",
+            configValueTypeToString(ExpectedType),
             YamlFile::configKeyPathToString(config_key_path),
             configValueTypeToString(value.getValueType())
          );
@@ -24,102 +24,34 @@ std::optional<std::string> VerifiedConfigAttributes::getString(const ConfigKeyPa
          YamlFile::configKeyPathToString(config_key_path),
          value.toString()
       );
-      return get<std::string>(value.value);
+      return std::get<T>(value.value);
    }
    return std::nullopt;
+}
+
+std::optional<std::string> VerifiedConfigAttributes::getString(const ConfigKeyPath& config_key_path) const {
+   return getValue<std::string, ConfigValueType::STRING>(config_key_path, config_values);
 }
 
 std::optional<std::filesystem::path> VerifiedConfigAttributes::getPath(
    const ConfigKeyPath& config_key_path
 ) const {
-   auto value_it = config_values.find(config_key_path);
-   if (value_it != config_values.end()) {
-      const ConfigValue& value = value_it->second;
-      if (value.getValueType() != ConfigValueType::PATH) {
-         SILO_PANIC(
-            "Called getPath on a ConfigKeyPath ('{}') that belongs to a value of another "
-            "type ({}).",
-            YamlFile::configKeyPathToString(config_key_path),
-            configValueTypeToString(value.getValueType())
-         );
-      }
-      SPDLOG_TRACE(
-         "Using for key `{}` the value {}",
-         YamlFile::configKeyPathToString(config_key_path),
-         value.toString()
-      );
-      return get<std::filesystem::path>(value.value);
-   }
-   return std::nullopt;
+   return getValue<std::filesystem::path, ConfigValueType::PATH>(config_key_path, config_values);
 }
 
 std::optional<int32_t> VerifiedConfigAttributes::getInt32(const ConfigKeyPath& config_key_path
 ) const {
-   auto value_it = config_values.find(config_key_path);
-   if (value_it != config_values.end()) {
-      const ConfigValue& value = value_it->second;
-      if (value.getValueType() != ConfigValueType::INT32) {
-         SILO_PANIC(
-            "Called getInt32 on a ConfigKeyPath ('{}') that belongs to a value of another "
-            "type ({}).",
-            YamlFile::configKeyPathToString(config_key_path),
-            configValueTypeToString(value.getValueType())
-         );
-      }
-      SPDLOG_TRACE(
-         "Using for key `{}` the value {}",
-         YamlFile::configKeyPathToString(config_key_path),
-         value.toString()
-      );
-      return get<int32_t>(value.value);
-   }
-   return std::nullopt;
+   return getValue<int32_t, ConfigValueType::INT32>(config_key_path, config_values);
 }
 
 std::optional<uint32_t> VerifiedConfigAttributes::getUint32(const ConfigKeyPath& config_key_path
 ) const {
-   auto value_it = config_values.find(config_key_path);
-   if (value_it != config_values.end()) {
-      const ConfigValue& value = value_it->second;
-      if (value.getValueType() != ConfigValueType::UINT32) {
-         SILO_PANIC(
-            "Called getUint32 on a ConfigKeyPath ('{}') that belongs to a value of another "
-            "type ({}).",
-            YamlFile::configKeyPathToString(config_key_path),
-            configValueTypeToString(value.getValueType())
-         );
-      }
-      SPDLOG_TRACE(
-         "Using for key `{}` the value {}",
-         YamlFile::configKeyPathToString(config_key_path),
-         value.toString()
-      );
-      return std::get<uint32_t>(value.value);
-   }
-   return std::nullopt;
+   return getValue<uint32_t, ConfigValueType::UINT32>(config_key_path, config_values);
 }
 
 std::optional<uint16_t> VerifiedConfigAttributes::getUint16(const ConfigKeyPath& config_key_path
 ) const {
-   auto value_it = config_values.find(config_key_path);
-   if (value_it != config_values.end()) {
-      const ConfigValue& value = value_it->second;
-      if (value.getValueType() != ConfigValueType::UINT16) {
-         SILO_PANIC(
-            "Called getUint16 on a ConfigKeyPath ('{}') that belongs to a value of another "
-            "type ({}).",
-            YamlFile::configKeyPathToString(config_key_path),
-            configValueTypeToString(value.getValueType())
-         );
-      }
-      SPDLOG_TRACE(
-         "Using for key `{}` the value {}",
-         YamlFile::configKeyPathToString(config_key_path),
-         value.toString()
-      );
-      return std::get<uint16_t>(value.value);
-   }
-   return std::nullopt;
+   return getValue<uint16_t, ConfigValueType::UINT16>(config_key_path, config_values);
 }
 
 std::optional<bool> VerifiedConfigAttributes::getBool(const ConfigKeyPath& config_key_path) const {
