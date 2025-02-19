44cda82dabc7cd45cd187b2277c00be9dee8930b
Alexander Taepper <alexander.taepper@gmail.com>
2025-02-19 16:15:40 +0100

    feat: call malloc_trim after all connections to the old database are finished

--- src/silo/api/active_database.cpp
+++ src/silo/api/active_database.cpp
@@ -1,5 +1,9 @@
 #include "silo/api/active_database.h"
 
+#if defined(__linux__)
+#include <malloc.h>
+#endif
+
 #include <atomic>
 #include <utility>
 
@@ -7,7 +11,7 @@
 #include "silo/database.h"
 
 namespace {
-void monitorReferenceCount(
+void monitorReferenceCountThenTrimAllocations(
    silo::DataVersion::Timestamp data_version,
    std::weak_ptr<silo::Database> weak_ptr
 ) {
@@ -24,6 +28,10 @@ void monitorReferenceCount(
             "No more references to the old database with version {} present. Stopping monitoring.",
             data_version.value
          );
+#if defined(__linux__)
+         SPDLOG_INFO("Manually invoking malloc_trim() to give back memory to OS.");
+         malloc_trim(0);
+#endif
          break;
       }
       std::this_thread::sleep_for(std::chrono::seconds(5));
@@ -37,7 +45,7 @@ void silo::api::ActiveDatabase::setActiveDatabase(silo::Database&& new_database)
    auto active_database = std::atomic_load(&database);
    if (active_database != nullptr) {
       std::thread monitorThread(
-         monitorReferenceCount,
+         monitorReferenceCountThenTrimAllocations,
          database->getDataVersionTimestamp(),
          std::weak_ptr<silo::Database>(active_database)
       );
