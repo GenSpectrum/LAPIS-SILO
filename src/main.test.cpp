2272f31c7cbc13456aeaf40cef5ea5280a5132cb
Alexander Taepper <alexander.taepper@gmail.com>
2025-06-03 09:22:09 +0200

    refactor: be more permissible of the working directory when running tests

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -1,19 +1,35 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include <filesystem>
+
 #include <spdlog/sinks/null_sink.h>
 #include <spdlog/spdlog.h>
-#include <filesystem>
 
 #include "silo/common/log.h"
 
-int main(int argc, char* argv[]) {
-   try {
-      if (!std::filesystem::exists("testBaseData/exampleDataset")) {
-         throw std::runtime_error("must be run in root of repository");
+int changeCwdToTestFolder() {
+   // Look for the test data directory (`testBaseData`) in the current directory and up to
+   // <search_depth> directories above the current directory. If found, change the current working
+   // directory to the directory containing the test data directory
+   size_t search_depth = 3;
+   std::filesystem::path candidate_directory = std::filesystem::current_path().string();
+   for (size_t i = 0; i < search_depth; i++, candidate_directory = candidate_directory / "..") {
+      if (std::filesystem::exists(candidate_directory / "testBaseData/exampleDataset")) {
+         std::filesystem::current_path(candidate_directory);
+         return 0;
       }
-   } catch (std::exception& e) {
-      SPDLOG_ERROR(e.what());
-      return 1;
+   }
+   SPDLOG_ERROR(
+      "Should be run in root of repository, got {} and could not find root by heuristics",
+      std::filesystem::current_path().string()
+   );
+   return 1;
+}
+
+int main(int argc, char* argv[]) {
+   if (auto exit = changeCwdToTestFolder()) {
+      return exit;
    }
    if (spdlog::get_level() > spdlog::level::debug) {
       spdlog::set_level(spdlog::level::trace);
