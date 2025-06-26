f8351c11dbe0a884daabeba897b99627d8c5d188
Alexander Taepper <alexander.taepper@gmail.com>
2025-06-26 10:16:10 +0200

    test: reproduction of invalid nested and

---
+++ src/silo/query_engine/filter/expressions/and.test.cpp
@@ -0,0 +1,139 @@
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <nlohmann/json.hpp>
+
+#include "silo/test/query_fixture.test.h"
+
+namespace {
+using silo::ReferenceGenomes;
+using silo::config::DatabaseConfig;
+using silo::config::ValueType;
+using silo::test::QueryTestData;
+using silo::test::QueryTestScenario;
+
+using boost::uuids::random_generator;
+
+nlohmann::json createData(const std::string& country, const std::string& date) {
+   static std::atomic_int id = 0;
+   const auto primary_key = id++;
+   std::string age = id % 2 == 0 ? "null" : fmt::format("{}", 3 * id + 4);
+   float coverage = 0.9;
+
+   return nlohmann::json::parse(fmt::format(
+      R"(
+{{
+   "metadata": {{
+      "primaryKey": "id_{}",
+      "country": "{}",
+      "age": {},
+      "coverage": {},
+      "date": "{}"
+   }},
+   "alignedNucleotideSequences": {{
+      "segment1": "ACGT"
+   }},
+   "unalignedNucleotideSequences": {{
+      "segment1": "ACGT"
+   }},
+   "nucleotideInsertions": {{
+      "segment1": ["2:A"]
+   }},
+   "alignedAminoAcidSequences": {{
+      "gene1": "V"
+   }},
+   "aminoAcidInsertions": {{
+      "gene1": []
+   }}
+}}
+)",
+      primary_key,
+      country,
+      age,
+      coverage,
+      date
+   ));
+}
+
+const auto DATABASE_CONFIG =
+   R"(
+defaultNucleotideSequence: "segment1"
+schema:
+  instanceName: "dummy name"
+  metadata:
+   - name: "primaryKey"
+     type: "string"
+   - name: "country"
+     type: "string"
+     generateIndex: true
+   - name: "age"
+     type: "int"
+   - name: "coverage"
+     type: "float"
+   - name: "date"
+     type: "date"
+  primaryKey: "primaryKey"
+)";
+
+const auto REFERENCE_GENOMES = ReferenceGenomes{
+   {{"segment1", "ATGCN"}},
+   {{"gene1", "M*"}},
+};
+
+const QueryTestData TEST_DATA{
+   .ndjson_input_data =
+      {createData("Switzerland", "2020-01-01"),
+       createData("Germany", "2000-03-07"),
+       createData("Germany", "2009-06-07"),
+       createData("Switzerland", "2003-07-02"),
+       createData("Switzerland", "2002-01-04"),
+       createData("Switzerland", "2001-12-07")},
+   .database_config = DATABASE_CONFIG,
+   .reference_genomes = REFERENCE_GENOMES
+};
+
+const QueryTestScenario NESTED_AND = {
+   .name = "NESTED_AND",
+   .query = nlohmann::json::parse(
+      R"(
+{
+  "action": {
+    "type": "Details"
+  },
+  "filterExpression": {
+      "children": [
+        {
+          "column": "date",
+          "from": "2009-01-01",
+          "to": null,
+          "type": "DateBetween"
+        },
+        {
+          "children": [
+            {
+              "column": "date",
+              "from": "2000-01-01",
+              "to": null,
+              "type": "DateBetween"
+            },
+            {
+              "column": "country",
+              "value": "Germany",
+              "type": "StringEquals"
+            }
+          ],
+          "type": "And"
+        }
+      ],
+      "type": "And"
+  }
+})"
+   ),
+   .expected_query_result = nlohmann::json::parse(
+      R"(
+[{"age":13,"country":"Germany","coverage":0.9,"date":"2009-06-07","primaryKey":"id_2"}])"
+   )
+};
+
+}  // namespace
+
+QUERY_TEST(And, TEST_DATA, ::testing::Values(NESTED_AND));
