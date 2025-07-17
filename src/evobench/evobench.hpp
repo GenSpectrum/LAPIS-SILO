9b29527472e52bd23506423ed1f706c6d928e812
Alexander Taepper <alexander.taepper@gmail.com>
2025-07-17 15:20:02 +0200

    fix initializing of `data`

--- src/evobench/evobench.hpp
+++ src/evobench/evobench.hpp
@@ -105,7 +105,7 @@ struct fixed_string {
    consteval fixed_string()
        : data{} {};
 
-   consteval fixed_string(std::string_view str) { std::copy_n(str.data(), str.size(), data); }
+   consteval fixed_string(std::string_view str) : data{} { std::copy_n(str.data(), str.size(), data); }
 
    constexpr operator const char*() const { return data; }
 };
