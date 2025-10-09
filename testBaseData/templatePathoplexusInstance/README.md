0d0504f1c692237360b9f490272696fc523df6de
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-10-09 12:31:25 +0200

    feat(silo): add template to run ppx instance

---
+++ testBaseData/templatePathoplexusInstance/README.md
@@ -0,0 +1,49 @@
+# Template Pathoplexus Instance
+
+This is a config for reproducing the current Pathoplexus LAPIS-SILO instances locally or in a docker container using the latest LAPIS/SILO version. The script requires passing the Pathoplexus organism as an argument, it will create a folder <ORGANISM> with the config required to run
+
+```bash
+configure.sh <ORGANISM>
+```
+
+To run SILO locally using this configuration:
+
+```bash
+cd <ORGANISM>
+../../../build/Release/silo preprocessing --database-config database_config.yaml --preprocessing-config preprocessing_config.yaml
+../../../build/Release/silo api --api-port 8093
+```
+
+To run SILO and LAPIS in a docker container you can run:
+
+```bash
+SILO_PORT=8093 LAPIS_PORT=8092 docker compose -f docker-compose.yml up
+```
+To test the instance you can send LAPIS queries in the form of:
+
+```
+curl http://localhost:8092/sample/unalignedNucleotideSequences
+```
+
+and SILO queries in the form of:
+
+```
+curl -X POST http://localhost:8093/query \
+  -H "Content-Type: application/json" \
+  -d '{
+      "action": {
+        "type": "Fasta",
+        "sequenceNames": ["unaligned_main"]
+      },
+      "filterExpression": {
+        "type": "True"
+      }
+  }'
+```
+
+To test thread-related issues it makes sense to limit the number of requests that can be handled by SILO using `--api-max-queued-http-connections` when running the SILO api.
+
+## Earlier SILO/LAPIS versions
+
+To use a version of SILO older than v0.9.0 use the `database_config.old.yaml` instead of the `database_config.yaml`, for versions older than v0.8 additionally use `get-released-data.ndjson.zst` instead of `get-released-data.modified.ndjson.zst`.
+
---
+++ testBaseData/templatePathoplexusInstance/configure.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ORGANISM="$1"
+
+OUTDIR="${ORGANISM}"
+mkdir -p "$OUTDIR"
+
+# Get data from Loculus backend
+echo "Fetching data for ${ORGANISM} ..."
+curl -X 'GET' \
+  'https://backend.pathoplexus.org/ebola-sudan/get-released-data?compression=ZSTD' \
+  -H 'accept: application/x-ndjson' -o "${ORGANISM}/get-released-data.ndjson.zst"
+
+# Run legacyNdjsonTransformer on data
+echo "Transforming data for ${ORGANISM} ..."
+cargo build --release --bin legacy-ndjson-transformer
+zstdcat "${ORGANISM}/get-released-data.ndjson.zst" | ./target/release/legacy-ndjson-transformer | zstd > "${ORGANISM}/get-released-data.transformed.ndjson.zst"
+
+# Get config files from LAPIS
+echo "Fetching config files for ${ORGANISM} ..."
+echo "Fetching database config ..."
+curl -X 'GET' \
+  'https://lapis.pathoplexus.org/${ORGANISM}/sample/databaseConfig?downloadAsFile=false' \
+  -H 'accept: application/json' -o "${ORGANISM}/database_config.yaml"
+echo "Fetching reference genome ..."
+curl -X 'GET' \
+  'https://lapis.pathoplexus.org/mpox/sample/referenceGenome?downloadAsFile=false' \
+  -H 'accept: application/json' -o "${ORGANISM}/reference_genome.yaml"
+
+echo "Parse database config to see if lineage definitions are required ..."
+names=$(yq '.schema.metadata[] | select(.generateLineageIndex == true or .generateLineageIndex == "true") | .name' file.yaml)
+for name in $names; do
+  echo "Fetching lineage definition for $name ..."
+  curl -s -X 'GET' \
+    "https://lapis.pathoplexus.org/${ORGANISM}/sample/lineageDefinition/${name}?downloadAsFile=false" \
+    -H 'accept: application/json' \
+    -o "${OUTDIR}/lineage_definition_${name}.yaml"
+done
+
+# For v.0.9.0 and later, we need to refactor the database config to the new lineage definition format
+echo "Refactor database config to new lineage definition format ..."
+yq '
+.schema.metadata |=
+  [
+    .[] |
+    select(.generateLineageIndex != false) |
+    if .generateLineageIndex == true then
+      .generateLineageIndex = "lineage_definition_" + .name
+    else
+      .
+    end
+  ]
+' "${ORGANISM}/database_config.yaml" > "${ORGANISM}/database_config.modified.yaml"
+mv "${ORGANISM}/database_config.yaml" "${ORGANISM}/database_config.old.yaml"
+mv "${ORGANISM}/database_config.modified.yaml" "${ORGANISM}/database_config.yaml"
+
+echo "Creating processing config ..."
+config_file="${OUTDIR}/processing_config.yaml"
+
+{
+  echo "inputDirectory: \"./\""
+  echo "outputDirectory: \"./output/\""
+  echo "ndjsonInputFilename: \"get-released-data.transformed.ndjson.zst\""
+  if (( ${#names[@]} > 0 )); then
+    echo "lineageDefinitionFilenames:"
+    for name in $names; do
+      echo "  - \"lineage_definition_${name}.yaml\""
+    done
+  fi
+  echo "referenceGenomeFilename: \"reference_genomes.json\""
+} > "$config_file"
+
+echo "Created ${config_file}"
+
+echo "Copying docker_compose_template.yml to ${OUTDIR}/docker_compose.yml ..."
+cp docker_compose_template.yml "${OUTDIR}/docker_compose.yml"
+
+echo "Done setting up ${OUTDIR}."
