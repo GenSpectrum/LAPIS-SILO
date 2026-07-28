62990c7d155f67d9c7d4007bc6820fe250ece5a6
Alexander Taepper <alexander.taepper@gmail.com>
2026-07-28 22:31:04 +0200

    ci: also allow `main` and `deps` as scopes

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -37,7 +37,7 @@ const Configuration = {
     "scope-enum": [
       RuleConfigSeverity.Error,
       "always",
-      [...trackedRootDirectories, ...siloSubdirectories, "silo"],
+      [...trackedRootDirectories, ...siloSubdirectories, "silo", "main", "deps"],
     ],
   },
 };
